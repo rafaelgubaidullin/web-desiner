0ec8dc8122a2ee55716148e14fbc822943839a2f
Rafael <rafdigital22@gmail.com>
2022-09-19 00:11:07 +0300

    responsive hero banner & layout section about-me
    add file _about-me.scss & code html scss 
    file _media.scss

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const burger = document.querySelector(".navigation__burger"),
-  hidden = document.querySelector(".navigation__hidden"),
-  menuClose = document.querySelector(".navigation__menu-close");
+const burger = document.querySelector(".navigation__burger");
+const hidden = document.querySelector(".navigation__hidden");
+const menuClose = document.querySelector(".navigation__menu-close");
+
 burger.addEventListener("click", () => hidden.classList.add("active"));
 
 menuClose.addEventListener("click", () => hidden.classList.remove("active"));
